c5c8464ad89b41749d9da53ea72482f511a2b251
何卓鹏 <hezhuopeng@cvte.com>
2018-11-25 19:55:38 +0800

    [optimie] 1.更新型号逻辑优化

--- app/pages/manager-goods-list/index.ts
+++ app/pages/manager-goods-list/index.ts
@@ -170,8 +170,8 @@ Page({
     onTab({ currentTarget }) {
         const { pid } = currentTarget.dataset;
         wx.navigateTo({
-            // url: `/pages/goods-detail/index?id=${pid}`
-            url: `/pages/manager-goods-detail/index?id=${pid}`
+            url: `/pages/goods-detail/index?id=${pid}`
+            // url: `/pages/manager-goods-detail/index?id=${pid}`
         });
     },
   
