9e09a5f712f7bbb9dd70f15a8792415970c19c15
何卓鹏 <hezhuopeng@cvte.com>
2018-12-10 08:40:20 +0800

    [optimize] 1.good-item-1

--- cloud/api-trip-enter/index.ts
+++ cloud/api-trip-enter/index.ts
@@ -4,6 +4,7 @@ import * as cloud from 'wx-server-sdk';
 cloud.init( );
 
 const db: DB.Database = cloud.database( );
+const _ = db.command;
 
 /**
  * @description 返回两个行程，一个在用/即将到来，另一个下一趟即将到来
@@ -21,10 +22,11 @@ export const main = async ( event, context) => {
 
     try {
 
-        // 按开始日期正序，获取最多2条已发布行程
+        // 按开始日期正序，获取最多2条已发布，未结束的行程
         const data$ = await db.collection('trip')
             .where({
-                published: true
+                published: true,
+                end_date: _.gt( new Date( ).getTime( ))
             })
             .limit( 2 )
             .orderBy('start_date', 'asc')
