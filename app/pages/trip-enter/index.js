f30a07996c069e3f1081d2b77a9111662fea838a
何卓鹏 <hezhuopeng@cvte.com>
2019-01-07 22:32:28 +0800

    .

--- app/pages/trip-enter/index.js
+++ app/pages/trip-enter/index.js
@@ -191,10 +191,11 @@ Page({
     /**
      * 用户点击右上角分享
      */
-    onShareAppMessage: function () {
+    onShareAppMessage: function ( ) {
         return {
-            title: '[微信红包]一起分享超值行程红包',
-            path: '/pages/trip-enter/index'
+            title: '[有人@你]跟我一起来拔草～',
+            path: '/pages/trip-enter/index',
+            imageUrl: 'https://global-1257764567.cos.ap-guangzhou.myqcloud.com/share.png'
         }
     }
 })
