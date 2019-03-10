9c62124ab3419bfe8ed3c8bf451c87b14b89ed3a
何卓鹏 <hezhuopeng@cvte.com>
2019-03-10 10:10:16 +0800

    .

--- app/pages/trip-detail/index.js
+++ app/pages/trip-detail/index.js
@@ -7,36 +7,89 @@ Page({
      * 页面的初始数据
      */
     data: {
+
         /** 行程id */
-        tid: ''
+        tid: '',
+
+        /** 加载状态 */
+        loading: true,
+
+        /** 等待拼团列表 */
+        waitPin: [ ],
+
+    },
+
+    /** 设置computed */
+    runComputed( ) {
+        computed( this, {
+
+            /** 等待拼团列表 */
+            waitPin$: function( ) {
+                const { waitPin } = this.data;
+                return waitPin.sort(( x, y ) => {
+                    return x.detail.saled - y.detail.saled;
+                });
+            }
+        });
     },
 
     /** 拉取等待拼团 */
     fetchWaitPin( tid ) {
-        http({
-            data: {
-                tid,
-                type: 'wait',
-                showUser: true
-            },
-            url: 'shopping-list_pin',
-            success: res => {
-                const { status, data } = res;
-                if ( status !== 200 ) { return; }
-            }
+        return new Promise(( resolve, reject ) => {
+            http({
+                data: {
+                    tid,
+                    type: 'wait',
+                    showUser: true
+                },
+                url: 'shopping-list_pin',
+                success: res => {
+                    const { status, data } = res;
+                    if ( status !== 200 ) {
+                        return reject( );
+                    }
+                    this.setData({ 
+                        waitPin: data
+                    });
+                    resolve( );
+                }
+            });
         })
+    },
+
+    /** 拉取已经拼团成功的商品列表 */
+    fetchPin( tid ) {
+        return new Promise(( resolve, reject ) => {
+            resolve( );
+        });
+    },
+
+    /** 拉取拼团、等待拼团 */
+    fetchAllPin( tid ) {
+        Promise.all([
+            this.fetchPin( tid ),
+            this.fetchWaitPin( tid )
+        ]).then(( ) => {
+            this.setData({
+                loading: false
+            })
+        }).catch( e => {
+
+        });
     },
 
     /**
      * 生命周期函数--监听页面加载
      */
     onLoad: function (options) {
+
+        this.runComputed( );
         const tid = options.id;
         if ( tid ) { 
             this.setData({
                 tid
             });
-            this.fetchWaitPin( tid )
+            this.fetchAllPin( tid )
         }
         /**
          * !请记得去掉这段代码
@@ -44,7 +97,7 @@ Page({
         this.setData({
             tid: 'XDGzG97E7L4wLIdu'
         });
-        this.fetchWaitPin( 'XDGzG97E7L4wLIdu' )
+        this.fetchAllPin( 'XDGzG97E7L4wLIdu' )
     },
 
     /**
