612777885cdea54042f827ebf69cbab8b0d4ef8e
何卓鹏 <hezhuopeng@cvte.com>
2019-03-13 13:26:47 +0800

    .

--- app/pages/search/index.js
+++ app/pages/search/index.js
@@ -1,4 +1,6 @@
 
+const storageKey = 'client-search';
+
 Page({
 
     /**
@@ -6,17 +8,64 @@ Page({
      */
     data: {
 
+        // 历史搜搜数据列表
+        history: [ ],
+
+        // 搜索
+        search: ''
+
     },
 
-    onSearch( e ) {
-        console.log( e );
+    /** 搜索 */
+    onSearch({ detail }) {
+        const search = detail;
+        this.addHistory( search );
+
+        // 搜索
+    },
+
+    /** 加入历史记录 */
+    addHistory( val ) {
+        const meta =  wx.getStorageSync( storageKey );
+        let temp = meta ? JSON.parse( meta ) : [ ];
+
+        const existedIndex = temp.findIndex( x => x === val );
+
+        // 存在该历史记录
+        if ( existedIndex !== -1 ) {
+            temp.splice( existedIndex, 1 );
+        } 
+
+        temp.unshift( val );
+        temp = temp.slice( 0, 6 );
+        
+        wx.setStorageSync( storageKey, JSON.stringify( temp ));
+    },
+
+    /** 初始化历史记录 */
+    initHistory( ) {
+        const meta =  wx.getStorageSync( storageKey );
+        this.setData({
+            history: meta ? JSON.parse( meta ) : [ ]
+        });
+    },
+
+    /** 选择历史记录 */
+    onSelectHistory({ currentTarget }) {
+        const { data } = currentTarget.dataset;
+        this.setData({
+            search: data
+        });
+        this.onSearch({
+            detail: data
+        });
     },
 
     /**
      * 生命周期函数--监听页面加载
      */
     onLoad: function (options) {
-
+        this.initHistory( );
     },
 
     /**
