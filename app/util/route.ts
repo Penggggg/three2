cc78944d1bc39c3b641df17d128ce7bc91a0d8b0
何卓鹏 <hezhuopeng@cvte.com>
2019-03-28 22:51:37 +0800

    .

--- app/util/route.ts
+++ app/util/route.ts
@@ -1,8 +1,36 @@
 const MAX_PAGES = 9;
 
+/** 根据url 返回param对象 */
+const makeQuery = ( url: string ) => {
+    let params = { };
+    const path = url.split('?')[ 1 ];
+
+    if ( !path ) {
+        return params;
+    }
+
+    const paramsArr = path.split('&');
+    paramsArr.map( paramsSet => {
+        params = Object.assign({ }, params, {
+            [ paramsSet.split('=')[ 0 ]]:  paramsSet.split('=')[ 1 ]
+        })
+    });
+    return params;
+};
+
+/** 
+ * ! 此函数有漏洞
+ * 判断两个params对象是否相等
+ */
+const judgeEqual = ( obj1: object, obj2: object ) => {
+    const key1 = Object.keys( obj1 );
+    const key2 = Object.keys( obj2 );
+    if ( key1.length !== key2.length ) { return; }
+    return !key1.some( keyx1 => obj1[ keyx1 ] !== obj2[ keyx1 ]);
+};
+
 /**
  * 
- * ! bug
  * 连续2个相同url，不同query，
  * A -> B 的时候，query参数可能有问题
  */
@@ -10,19 +38,53 @@ export const navTo = ( url: string ) => {
 
         const pages = getCurrentPages( );
         const pageLen = pages.length;
-        const existedIndex = pages.findIndex( x => `/${x.route}` === url.split('?') [0 ]);
+        const params = makeQuery( url );
+        const existedTarget: any = pages.find( x => `/${x.route}` === url.split('?') [ 0 ]);
+        const existedIndex = pages.findIndex( x => `/${x.route}` === url.split('?') [ 0 ]);
 
-        console.log( pages.map( x => x.route ), pageLen, existedIndex )
+        // 如果找到历史路由
+        if ( !!existedTarget ) {
 
-        if ( existedIndex !== -1 ) {
-            wx.navigateBack({
-                delta: pageLen - existedIndex - 1
-            });
+            const isParamEqual = judgeEqual( params, existedTarget.options );
+
+            if ( isParamEqual ) {
+                // 参数匹配的情况下，立马返回
+                wx.navigateBack({
+                    delta: pageLen - existedIndex - 1
+                });
+
+            // url匹配，但是参数不匹配
+            } else {
+                // wx.navigateBack({
+                //     delta: pageLen - existedIndex
+                // });
+                // wx.navigateTo({
+                //     url
+                // });
+
+                // 找不到，且不超过max
+                if ( pageLen < MAX_PAGES ) {
+                    wx.navigateTo({
+                        url
+                    });
+
+                // 找不到，且超过了max
+                } else {
+                    wx.redirectTo({
+                        url
+                    });
+                }
+            }
+
         } else {
+
+            // 找不到，且不超过max
             if ( pageLen < MAX_PAGES ) {
                 wx.navigateTo({
                     url
                 });
+
+            // 找不到，且超过了max
             } else {
                 wx.redirectTo({
                     url
