d350a7e4a96c26d1921ffc8c8fc7a361a6e7d2fe
何卓鹏 <hezhuopeng@cvte.com>
2019-06-18 10:08:58 +0800

    .

---
+++ cloud/timer-order/form-ids/index.ts
@@ -0,0 +1,37 @@
+import * as cloud from 'wx-server-sdk';
+
+cloud.init({
+    env: process.env.cloud
+});
+
+const db: DB.Database = cloud.database( );
+const _ = db.command;
+
+const FORM_ID_OVERTIME = 7 * 24 * 60 * 60 * 1000;
+
+/**
+ * @description
+ * form-ids1: 所有超过7天的formid是无效的，自动删除掉
+ */
+export const clearFormIds = async ( ) => {
+    try {
+
+        const find$ = await db.collection('form-ids')
+            .where({
+                creatTime: _.lte( new Date( ).getTime( ) - FORM_ID_OVERTIME )
+            })
+            .get( );
+
+        await Promise.all(
+            find$.data.map( x => 
+                db.collection('form-ids')
+                    .doc( String( x._id ))
+                    .remove( )
+            )
+        );
+        return { status: 200 }
+    } catch ( e ) {
+        console.log('!!!!定时器clearFormIds错误',)
+        return { status: 500 }
+    }
+};
--- cloud/timer-order/index.ts
+++ cloud/timer-order/index.ts
@@ -1,7 +1,8 @@
 import * as cloud from 'wx-server-sdk';
+import { overtimeTrip } from './trip';
+import { clearFormIds } from './form-ids';
 import { overtime, payedFix, priceFix, payLastFix } from './order';
 import { catchLostOrders, removeUselessOrders } from './shopping-list';
-import { overtimeTrip } from './trip';
 
 cloud.init({
     env: process.env.cloud
@@ -27,6 +28,7 @@ export const main = async ( event, context ) => {
         await overtimeTrip( );
         await catchLostOrders( );
         await removeUselessOrders( );
+        await clearFormIds( );
         
         return {
             status: 200
