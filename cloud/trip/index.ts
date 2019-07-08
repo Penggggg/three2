28513c39302c8e55d60057fe95027434000adc88
何卓鹏 <hezhuopeng@cvte.com>
2019-07-08 22:17:55 +0800

    .

--- cloud/trip/index.ts
+++ cloud/trip/index.ts
@@ -396,7 +396,7 @@ export const main = async ( event, context ) => {
                                     openid: member.openid,
                                     type: 'trip',
                                     page: 'pages/manager-trip-list/index',
-                                    texts: [`${title}`, `发布成功！已开通新订单推送`]
+                                    texts: [`${title}`, `行程已推送给客户！开通了新订单推送`]
                                 }
                             }
                         });
