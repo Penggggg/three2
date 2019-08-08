822d32e388f67fe2521f81a347eb735251a7f49c
何卓鹏 <hezhuopeng@cvte.com>
2019-08-08 15:03:49 +0800

    .

---
+++ app/container/save-wma/index.js
@@ -0,0 +1,58 @@
+
+const storageKey = 'save-wma-last-time';
+
+Component({
+    /**
+     * 组件的属性列表
+     */
+    properties: {
+        
+    },
+
+    /**
+     * 组件的初始数据
+     */
+    data: {
+        show: false
+    },
+
+    /**
+     * 组件的方法列表
+     */
+    methods: {
+        toggle( ) {
+            const { show } = this.data;
+            this.setData({
+                show: !show
+            })
+        },
+
+        check( ) {
+
+            const delta = 5 * 24 * 60 * 60 * 10000;
+
+            const openAndClose = ( ) => {
+                this.toggle( );
+                setTimeout(( ) => {
+                    this.toggle( );
+                }, 10000 );
+            }
+
+            setTimeout(( ) => {
+                const last = Number( wx.getStorageSync( storageKey ));
+                if ( !last ) {
+                    openAndClose( );
+                } else if ( Date.now( ) - last > delta ) {
+                    // 多于5天的时候再显示一下
+                    wx.setStorageSync( storageKey, String( Date.now( )));
+                    openAndClose( );
+                }
+            }, 5000 );
+        }
+    },
+
+    attached: function( ) {
+        this.check( );
+    }
+
+})
