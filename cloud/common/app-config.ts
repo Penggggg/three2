8657613b61aab9c36363c60fbfc895d0f987de71
何卓鹏 <hezhuopeng@cvte.com>
2019-08-09 14:50:16 +0800

    .

--- cloud/common/app-config.ts
+++ cloud/common/app-config.ts
@@ -20,5 +20,37 @@ export default [
         desc: '积分推广抵扣比例',
         type: 'push-integral-money-rate',
         value: 0.1
+    }, {
+        desc: '用户等级1',
+        type: 'user-level-one',
+        value: 0
+    }, {
+        desc: '用户等级2',
+        type: 'user-level-two',
+        value: 120
+    }, {
+        desc: '用户等级3',
+        type: 'user-level-three',
+        value: 500
+    }, {
+        desc: '签到奖励1',
+        type: 'sign-gift-one',
+        value: [ 0.53, 0.58, 0.62, 0.68, 0.73, 0.78, 0.88 ]
+    }, {
+        desc: '签到奖励2',
+        type: 'sign-gift-two',
+        value: [ 0.58, 0.78, 1.06, 1.24, 1.46, 1.7, 1.98 ]
+    }, {
+        desc: '签到奖励3',
+        type: 'sign-gift-three',
+        value: [ 0.66, 1.08, 1.34, 1.84, 2.32, 2.68, 2.88 ]
+    }, {
+        desc: '个人IP名称',
+        type: 'ip-name',
+        value: ''
+    }, {
+        desc: 'ip的头像',
+        type: 'ip-avatar',
+        value: ''
     }
 ]
