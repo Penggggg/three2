7ab16507d886530dda5ec547b477c997604eb735
何卓鹏 <hezhuopeng@cvte.com>
2019-08-13 23:25:39 +0800

    .

--- app/pages/good-activity/index.js
+++ app/pages/good-activity/index.js
@@ -1,66 +1,113 @@
-// app/pages/good-activity/index.js
+const { http } = require('../../util/http.js');
+const { navTo } = require('../../util/route.js');
+const { computed } = require('../../lib/vuefy/index.js');
+const { delayeringGood } = require('../../util/goods.js');
+const { createFormId } = require('../../util/form-id');
+const app = getApp( );
+
 Page({
 
-  /**
-   * 页面的初始数据
-   */
-  data: {
+    /**
+     * 页面的初始数据
+     */
+    data: {
 
-  },
+        // banner文字
+        bannerTips: [
+            '买就赚',
+            '限时的特价'
+        ],
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
+        // ip
+        ipName: '',
 
-  },
+        // ip
+        ipAvatar: ''
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
+    },
 
-  },
+    runComputed( ) {
+        computed( this, {
+            // 热门推荐 + 活动标志 
+            bannerTips$: function( ) {
+                const { bannerTips, ipName } = this.data;
+                if ( !ipName ) {
+                    return bannerTips
+                } else {
+                    return [
+                        `${ipName}的`,
+                        `买就赚特价`
+                    ]
+                }
+            }
+        });
+    },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
+    /** 全局数据 */
+    watchRole( ) {
+        app.watch$('appConfig', ( val ) => {
+            if ( !val ) { return; }
+            this.setData({
+                ipName: val['ip-name'],
+                ipAvatar: val['ip-avatar']
+            })
+        });
+    },
 
-  },
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad: function (options) {
+        this.watchRole( );
+        this.runComputed( );
+    },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
+    /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+    onReady: function () {
 
-  },
+    },
 
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow: function () {
 
-  },
+    },
 
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
+    /**
+     * 生命周期函数--监听页面隐藏
+     */
+    onHide: function () {
 
-  },
+    },
 
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
+    /**
+     * 生命周期函数--监听页面卸载
+     */
+    onUnload: function () {
 
-  },
+    },
 
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
+    /**
+     * 页面相关事件处理函数--监听用户下拉动作
+     */
+    onPullDownRefresh: function () {
 
-  }
+    },
+
+    /**
+     * 页面上拉触底事件的处理函数
+     */
+    onReachBottom: function () {
+
+    },
+
+    /**
+     * 用户点击右上角分享
+     */
+    onShareAppMessage: function () {
+
+    }
 })
