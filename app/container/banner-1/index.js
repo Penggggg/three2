5fd5fe9c2d522d0b098465e03756e5e0ee194313
何卓鹏 <hezhuopeng@cvte.com>
2019-08-13 23:04:14 +0800

    .

---
+++ app/container/banner-1/index.js
@@ -0,0 +1,34 @@
+
+Component({
+    /**
+     * 组件的属性列表
+     */
+    properties: {
+        title1: {
+            type: String,
+            value: ''
+        },
+        title2: {
+            type: String,
+            value: ''
+        },
+        img: {
+            type: String,
+            value: ''
+        }
+    },
+
+    /**
+     * 组件的初始数据
+     */
+    data: {
+
+    },
+
+    /**
+     * 组件的方法列表
+     */
+    methods: {
+
+    }
+})
--- app/pages/good-new/index.js
+++ app/pages/good-new/index.js
@@ -1,3 +1,9 @@
+const { http } = require('../../util/http.js');
+const { navTo } = require('../../util/route.js');
+const { computed } = require('../../lib/vuefy/index.js');
+const { delayeringGood } = require('../../util/goods.js');
+const { createFormId } = require('../../util/form-id');
+const app = getApp( );
 
 Page({
 
@@ -6,13 +12,54 @@ Page({
      */
     data: {
 
+        // banner文字
+        bannerTips: [
+            '发现',
+            '美好新品'
+        ],
+
+        // ip
+        ipName: '',
+
+        // ip
+        ipAvatar: ''
+
+    },
+
+    runComputed( ) {
+        computed( this, {
+            // 热门推荐 + 活动标志 
+            bannerTips$: function( ) {
+                const { bannerTips, ipName } = this.data;
+                if ( !ipName ) {
+                    return bannerTips
+                } else {
+                    return [
+                        `${ipName}`,
+                        `为你选新品`
+                    ]
+                }
+            }
+        });
+    },
+
+    /** 全局数据 */
+    watchRole( ) {
+        app.watch$('appConfig', ( val ) => {
+            if ( !val ) { return; }
+            this.setData({
+                ipName: val['ip-name'],
+                ipAvatar: val['ip-avatar']
+            })
+        });
     },
 
     /**
      * 生命周期函数--监听页面加载
      */
     onLoad: function (options) {
-
+        this.watchRole( );
+        this.runComputed( );
     },
 
     /**
