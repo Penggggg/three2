04423eec9f8d077591624729b275c7832f6a923c
何卓鹏 <hezhuopeng@cvte.com>
2019-12-05 11:18:42 +0800

    [opt]

--- app/components/icon-question/index.js
+++ app/components/icon-question/index.js
@@ -8,14 +8,27 @@ Component({
      * 组件的属性列表
      */
     properties: {
+        /**
+         * 颜色
+         */
         color: {
             type: String,
             value: '#6cd6d7'
         },
+        /**
+         * 尺寸
+         */
         size: {
             type: Number,
             value: 22,
             observer: 'onSize'
+        },
+        /**
+         * 颜色反转
+         */
+        invert: {
+            type: Boolean,
+            value: false
         }
     },
 
