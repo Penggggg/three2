f8b48be6cf9151966b4bbda83d43d9dd1384a274
何卓鹏 <hezhuopeng@cvte.com>
2020-01-10 15:17:53 +0800

    [opt]

--- cloud/timer-order/subscribe-push/config.ts
+++ cloud/timer-order/subscribe-push/config.ts
@@ -2,36 +2,36 @@ export const subscribe_templates = {
     buyPin: {
         textKeys: ['thing4', 'thing1'],
         desc: '拼团成功通知	温馨提示、商品名称',
-        id: 'yBBlVH0qvipvzSr93d-Own-cYkTnATjUBhd_Qi1L2vc'
+        id: '1dnxrEiIuypJ-EIzjCh0tcChobs4z2j2DC0xTJXcrrY'
     },
     hongbao: {
         textKeys: ['thing9', 'thing7'],
         desc: '开奖结果通知：开奖结果、开奖内容',
-        id: 'rau9z8QU48gWel5Jn8hLt6-ChVWi8puo8E_QXR1CgMc'
+        id: 'CKyZDgBMwNGJ2D03Te61rd7yCTmUGh0r9FJ4T7l93uE'
     },
     waitPin: {
         textKeys: ['thing4', 'thing1'],
         desc: '拼团进度通知	温馨提示、商品名称',
-        id: 'CC0DCPLC3SFVr1Im7ODEhRnlyMZVB8RIJSmptmLmC_U'
+        id: '93URVZtalBNld8_lBmqXq1juJ1QuBC3B9UOxt7krkJE'
     },
     buy: {
         textKeys: ['thing7', 'thing5'],
         desc: '订单状态通知： 订单状态、备注',
-        id: 'hAU2bG9eeGDr0KeOIcM_Sq0JbIlcA6Jfcex2HhX8OWI'
+        id: 'Beuqu_Y-PZRV_pCWo_k-G0ObHrsuLzvbnCGMjVuIDsw'
     },
     trip: {
         textKeys: ['thing11', 'thing6'],
         desc: '活动开始提醒：温馨提示、活动内容',
-        id: 'u91Cqoo76phn_0o5N_Jdqz62rry4UdyIR6SyjRsZy0w'
+        id: '1M6wamoJOrCEXra6xnit8KQWxxft7y0ULTASAEyLXYY'
     },
     getMoney: {
         textKeys: ['thing3', 'thing1'],
         desc: '尾款支付提醒	温馨提示、商品清单',
-        id: 'is_8EPAO0WvaKoG7eDdLcBRZdGDowX2sgnmIAGGhOY0'
+        id: '7o0xA_wJtDyZylhFgVQe4u0M2HdoSfjTjS7G5dbY1lw'
     }, 
     newOrder: {
         textKeys: ['thing8', 'thing4'],
         desc: '新订单提醒: 订单状态、购买商品',
-        id: 'Onl9TTI33fTvg5K7AHydhHjk_NPmFLDGsMMxcDXE2rk'
+        id: 'vOxV7Vn3i2Naggiu0bAe2eenEJ-vP9uqppKJFKuwnn4'
     }
 };
